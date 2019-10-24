8063010a8165188f52a248dc1cbb187d99f5384f
Henry Jimenez Ramirez <henrry.jimenez@bdtravelsolution.com>
2019-10-24 18:03:25 -0500

    beta@0

---
+++ 2020/web/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>
+      Flisol Cancún <br>
+      2020
+    </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss">
+  .home {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'IBM Plex Sans', sans-serif;
+    color: #FFF;
+    font-weight: bold;
+    letter-spacing: 1.3rem;
+    text-align: center;
+    line-height: 1.6;
+  }
+</style>

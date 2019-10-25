d43e70aabdb59ffe7d5daf7500fff223248ef23e
Henry Jimenez Ramirez <henrry.jimenez@bdtravelsolution.com>
2019-10-25 18:07:16 -0500

    banner

---
+++ 2020/web/src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="banner">
+    <div class="date">2019</div>
+    <h1 class="title">Flisol Cancún 2019</h1>
+    <p>Evento de difusión de Software Libre más grande en Latinoamérica y está dirigido a todo tipo de público</p>
+    <p>edición Cancún, Quintana Roo - México</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Banner"
+};
+</script>
--- 2020/web/src/views/Home.vue
+++ 2020/web/src/views/Home.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="home">
-    <h1>
-      Flisol Cancún <br>
-      2020
-    </h1>
+    <banner></banner>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '@/components/Banner.vue'
 
 export default {
-  name: 'home'
-}
-</script>
-
-<style lang="scss">
-  .home {
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-family: 'IBM Plex Sans', sans-serif;
-    color: #FFF;
-    font-weight: bold;
-    letter-spacing: 1.3rem;
-    text-align: center;
-    line-height: 1.6;
+  name: "home", 
+  components: {
+    Banner
   }
-</style>
+};
+</script>

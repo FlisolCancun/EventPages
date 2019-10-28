c7cec69145c0080133bb8d7a2097fb64cdc3d75f
Henry Jimenez Ramirez <henrry.jimenez@bdtravelsolution.com>
2019-10-28 17:02:00 -0500

    Updates

--- 2020/web/src/components/Banner.vue
+++ 2020/web/src/components/Banner.vue
@@ -1,9 +1,14 @@
 <template>
   <section class="banner">
-    <div class="date">2019</div>
-    <h1 class="title">Flisol Cancún 2019</h1>
-    <p>Evento de difusión de Software Libre más grande en Latinoamérica y está dirigido a todo tipo de público</p>
-    <p>edición Cancún, Quintana Roo - México</p>
+    <div class="container">
+      <img class="banner-logo" src="../assets/images/logo.png" alt="">
+      <h1 class="title">Flisol Cancun</h1>
+      <p class="extract">Evento de difusión de Software Libre más grande en Latinoamérica y está dirigido a todo tipo de público</p>
+      <div class="info">
+        <p>11 de Abril, 2020</p>
+        <p>Cancún, México</p>
+      </div>
+    </div>
   </section>
 </template>
 

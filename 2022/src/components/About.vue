0237898c72efbe177f494c151e2fc42ed2ef9585
Henry Raygan <henryraygan@MacBook-Pro-de-Henry.local>
2022-03-24 19:51:15 -0500

    Add Banner

---
+++ 2022/src/components/About.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="l-wrapper">
+    <div class="about-container">
+      <h2 class="works__title js-move-up">Acerca de</h2>
+      <div class="about-container__content">
+        <p class="works__description">
+          El festival latinoamericano de instalación de software libre “FLISOL”
+          es el evento de difusión orientado a la filosofía del software libre
+          más grande de Latinoamérica. Está dirigido a todo tipo de personas
+          estudiantes, profesores, ingenieros, entusiastas y público en general.
+          El evento se realiza cada año desde 2005, adoptando el 4to sábado de
+          abril de cada año para su festejo. El principal objetivo es promover
+          el uso del software libre ilustrando a la audiencia su filosofía,
+          alcance, desarrollo e impacto en diferentes aspectos de la sociedad.
+          La entrada al evento es
+          <strong class="color-accent">totalmente gratuita</strong> para todo el
+          público. La organización del evento es llevada a cabo por medio de un
+          grupo de entusiastas preocupados por promover el uso del software y de
+          sus cuatro libertades basadas en la ética y el desarrollo de la
+          sociedad para el conocimiento.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.about-container__content {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+}
+.works__description {
+  margin: 0;
+  font-size: 40px;
+  line-height: 50px;
+  letter-spacing: 0;
+  grid-column: 2 / -3;
+  padding: 1rem;
+}
+</style>
--- 2022/src/views/Home.vue
+++ 2022/src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="l-section">
+      <banner></banner>
+    </div>
+    <div class="l-section">
+      <about></about>
+    </div>
+    <div class="l-section">
+      <schedule></schedule>
+    </div>
+    <div class="l-section">
+      <footer></footer>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import Banner from "@/components/Banner.vue";
+import About from "@/components/About.vue";
+import Schedule from "@/components/Schedule.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Banner,
+    About,
+    Schedule,
+    Footer,
+  },
+};
+</script>
+<style>
+body {
+  font-family: resist, sans-serif;
+  overflow: hidden;
+}
+
+.l-section {
+  width: 100vw;
+  height: 100vh;
+}
+
+@media screen and (min-width: 1024px) {
+  .works__title {
+    font-size: 5rem;
   }
 }
-</script>
+.works__title {
+  margin: 0;
+  font-size: 5rem;
+  font-weight: bold;
+  line-height: 1.75;
+}
+</style>
